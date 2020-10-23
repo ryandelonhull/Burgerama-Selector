0c3ad1c27dce2ce70f1357512698665557521c6c
Ryan Hull <ryandelonhull@gmail.com>
2020-10-23 12:49:55 -0400

    change heroku connection

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);   
 } else {
     connection = mysql.createConnection({
-        host: "sm9j2j5q6c8bpgyq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        user: "zrn2xgdx7wpaay29",
-        password: "nmhpe3hu89mrhvqw",
-        database: "f2gqjerp3fjyrsq4"
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burgers_db"
     })
 };
 
