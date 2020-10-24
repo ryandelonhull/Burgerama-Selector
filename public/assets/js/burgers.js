16cad7d47b8aa222ff87c78c034cb2f8d5e80157
Ryan Hull <ryandelonhull@gmail.com>
2020-10-24 00:43:43 -0400

    finish css and handlebars

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,50 +1,38 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-    $(".btn-devour").on("click", function(event) {
-      var id = $(this).data("id");
+$(function () {
+  $(".btn-devour").on("click", function (event) {
+    var id = $(this).data("id");
 
-  
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: {devoured: true}
-      }).then(
-        function() {
-          console.log("devoured burger", id);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-    function validate(burger){
-        if (burger.burger_name && burger.burger_name.length > 0) {
-            return true;
-        }
-        $("#burger_name").addClass("invalid");
-        return false;
-    }
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var newBurger = {
-        burger_name: $("#burger_name").val().trim(),
-        devoured: false
-      };
-      if (!validate(newBurger)) {
-          return;
-      };
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: { devoured: true },
+    }).then(function () {
+      console.log("devoured burger", id);
+      location.reload();
     });
   });
-  
+  function validate(burger) {
+    if (burger.burger_name && burger.burger_name.length > 0) {
+      return true;
+    }
+    $("#burger_name").addClass("invalid");
+    return false;
+  }
+  $(".create-form").on("submit", function (event) {
+    event.preventDefault();
+    var newBurger = {
+      burger_name: $("#burger_name").val().trim(),
+      devoured: false,
+    };
+    if (!validate(newBurger)) {
+      return;
+    }
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      console.log("created new burger");
+      location.reload();
+    });
+  });
+});
